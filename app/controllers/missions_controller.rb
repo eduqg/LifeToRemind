b64c866305a41deb97f90860ae02c6c344b274f0
eduqg <eduardoqgomes@gmail.com>
2019-04-22 13:36:30 -0300

    Adding crud to missions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,49 @@
 class MissionsController < ApplicationController
+  before_action :set_mission, only: [:edit, :update, :destroy]
   def index
     @missions = Mission.all
+  end
+
+  def new
+    @mission = Mission.new
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    if @mission.update mission_params
+      flash[:notice] = "Missão atualizada com sucesso!"
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @mission = Mission.new mission_params
+
+    if @mission.save
+      flash[:notice] = "Missão criada com sucesso"
+      redirect_to root_url
+    else
+      render :new
+    end 
+  end
+
+  def destroy
+    @mission.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def mission_params
+    mission = params.require(:mission).permit(:purpose_of_life, :who_am_i, :why_exist)
+  end
+
+  def set_mission
+    @mission = Mission.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :missions, only: [:new, :create, :destroy, :edit, :update]
   root to: "missions#index"
 
 end
