63cdf70d3303df06545f88f16dba75e88ae03158
eduqg <eduardoqgomes@gmail.com>
2019-04-29 11:38:51 -0300

    Destroy dependens to plans and missions

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ApplicationRecord
   belongs_to :user
-  has_many :missions
+  has_many :missions, dependent: :destroy
   validates :life_objective, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :plans
+  has_many :plans, dependent: :destroy
 end
