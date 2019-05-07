90a0654ede6b321d42c137482fc7f00fb849722e
eduqg <eduardoqgomes@gmail.com>
2019-05-06 22:23:21 -0300

    Redirect to plans path after sign_in and sign_up

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,4 +26,7 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    plans_path
+  end
 end
