c856166f1a2d8078e7e79b31f0bdfb621f434acd
eduqg <eduardoqgomes@gmail.com>
2019-05-09 19:33:39 -0300

    Removing assets digest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(*.png *.gif *.jpg *.ico *.svg *.eot *.ttf *.woff *.woff2)
-
-Rails.application.config.assets.digest = true
