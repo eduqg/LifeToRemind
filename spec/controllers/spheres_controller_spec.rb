9d99338f6e4694bd34075b8d7b5f0874d4f8e8d3
eduqg <eduardoqgomes@gmail.com>
2019-05-21 18:52:35 -0300

    Generating spec controller files

---
+++ spec/controllers/spheres_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe SpheresController, type: :controller do
+
+end
---
+++ spec/controllers/swotparts_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe SwotpartsController, type: :controller do
+
+end
