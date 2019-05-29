a0e3ec43e8ab4444a6970c16f02f793d9aaad945
eduqg <eduardoqgomes@gmail.com>
2019-05-28 23:43:11 -0300

    Refactoring to user select_color form anywhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def select_color(progress)
+    if progress < 30
+      bar_color = @chartColors[0]
+    elsif progress > 31 and progress < 60
+      bar_color = @chartColors[1]
+    else
+      bar_color = @chartColors[2]
+    end
+    bar_color
+  end
 end
