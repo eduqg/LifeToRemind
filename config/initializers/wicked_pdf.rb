49e5ef9659b1a01072ffd9bc7f4b99a8f820986e
eduqg <eduardoqgomes@gmail.com>
2019-05-30 22:12:40 -0300

    Reverting change

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
 WickedPdf.config = {
-    exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf_linux_amd64"
+    :exe_path => "#{ENV['GEM_HOME']}/bin/wkhtmltopdf"
 }
