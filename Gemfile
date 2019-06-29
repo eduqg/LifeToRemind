e2dc49d83c6e847e7b4eeabc2be2e2eb57418ddf
eduqg <eduardoqgomes@gmail.com>
2019-06-29 16:35:01 -0300

    Confirm message to vision, swotpart, csf and bundle downgrade

--- Gemfile
+++ Gemfile
@@ -85,7 +85,7 @@ group :test do
   gem 'capybara', '~>3.8'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
+  gem 'webdrivers', '~> 4.0'
   gem 'database_cleaner'
   gem 'factory_bot_rails'
   gem 'faker'
--- app/assets/javascripts/steps/step_check.js
+++ app/assets/javascripts/steps/step_check.js
@@ -1,3 +1,18 @@
+function checkSwotAttributes() {
+    if ($('#field-Força').val() ||
+        $('#field-Fraqueza').val() ||
+        $('#field-Oportunidade').val() ||
+        $('#field-Ameaça').val()) {
+        if(confirm("Você deseja prosseguir sem salvar os campos adicionados?")) {
+            window.location.replace("../missions/new");
+        } else {
+            // Stay on the page to user save content
+        }
+    } else {
+        window.location.replace("../missions/new");
+    }
+};
+
 function checkMissionAttributes() {
     if ($('#mission_who_am_i').val() ||
         $('#mission_why_exist').val() ||
@@ -9,5 +24,34 @@ function checkMissionAttributes() {
         }
     } else {
         window.location.replace("../visions/new");
+    }
+};
+
+function checkVisionAttributes() {
+    if ($('#vision_where_im_going').val() ||
+        $('#vision_where_arrive').val() ||
+        $('#vision_how_complete_mission').val()) {
+        if(confirm("Você deseja prosseguir sem salvar sua visão?")) {
+            window.location.replace("../values/new");
+        } else {
+            // Stay on the page to user save content
+        }
+    } else {
+        window.location.replace("../values/new");
+    }
+};
+
+function checkCsfAttributes() {
+    if ($('#csf_what_makes_me_unique').val() ||
+        $('#csf_best_attributes').val() ||
+        $('#csf_essential_atributes').val() ||
+        $('#csf_health_factors').val()) {
+        if(confirm("Você deseja prosseguir sem salvar seus fatores críticos de sucesso?")) {
+            window.location.replace("../spheres/new");
+        } else {
+            // Stay on the page to user save content
+        }
+    } else {
+        window.location.replace("../spheres/new");
     }
 };
