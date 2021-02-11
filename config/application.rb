f82985a4bc196c4bc47ff3c848db8e8e235ad55d
eduqg <eduardoqgomes@gmail.com>
2021-02-11 18:03:43 -0300

    Config updates

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+HOSTNAME = ENV['HOSTNAME']
+
 module Ltr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
